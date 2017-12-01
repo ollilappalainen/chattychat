6b1fe98310ff77d6edd690c3a092700c0682c6d5
Olli Lappalainen <masterhillo@gmail.com>
2017-12-02 00:10:32 +0200

    mmm

--- app/App.jsx
+++ app/App.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
 //Custom imports
 import { Message } from "./models/Message";
@@ -17,23 +18,55 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       message: 'No Messages.',
+      messages: [],
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleMessageSend = this.handleMessageSend.bind(this);
   }
 
-  handleChange = message => event => {
+  messages = [];
+  
+  handleChange(event) {
     this.setState({
-      [message]: event.target.value,
+      message: event.target.value,
     });
   }
 
-  handleKeyPress = message => event => {
-    if (event.charCode == 13) {
-      event.preventDefault();
-      event.stopPropagation();
+  handleEnterPress = (event) => {
+    if (event.charCode === 13) {
+      const message = this.state.message;
+      const date = JSON.stringify(Date());
+      const username = 'Pekka';
+      let messages = this.messages;
+
+      messages.push({
+        sent: date,
+        username: username,
+        message: message
+      });
+
       this.setState({
-        [messages]: event.target.value,
+        messages: messages,
       });
     }
+  }
+
+  handleMessageSend = (event) => {
+    const message = this.state.message;
+    const date = JSON.stringify(Date());
+    const username = 'Pekka';
+    let messages = [];
+
+    messages.push({
+      sent: date,
+      username: username,
+      message: message
+    });
+
+    this.setState({
+      messages: messages,
+    });
   }
 
   render() {
@@ -45,9 +78,7 @@ export default class App extends React.Component {
           </Grid>
           <Grid item xs={10}>
             <Paper className="message-container">
-              <Paper className="receivedMessages">
-                <h2>{this.state.message}</h2>
-              </Paper>
+              <MessagesOutput messages={this.state.messages} />
             </Paper>
           </Grid>
           <Grid item xs={2}>
@@ -56,21 +87,64 @@ export default class App extends React.Component {
           <Grid item xs={2}>
             <Paper className="username-box">asdasdasd</Paper>
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={8}>
             <Paper className="message-input-box">
               <div className="message-input">
                 <TextField
                   id="message"
                   label="Message"
-                  onKeyPress={this.handleKeyPress('message')}
+                  autoComplete="off"
+                  onChange={this.handleChange}
+                  onKeyPress={this.handleEnterPress}
                   fullWidth>
-
                 </TextField>
               </div>              
             </Paper>
           </Grid>
+          <Grid item xs={2}>
+            <Button type="submit" color="primary" onClick={this.handleMessageSend}>SEND</Button>
+          </Grid>
         </Grid>
       </div>
     );
+  }
+}
+
+export class MessagesOutput extends React.Component {
+  constructor() {
+    super();
+
+    this.updateScroll = this.updateScroll.bind(this);
+  }
+
+  updateScroll(){
+    let element = document.getElementById("messagelist");
+    element.scrollTop = element.scrollHeight;
+  }
+  
+  componentDidMount() {
+    setInterval(this.updateScroll, 500);
+  }
+
+  render() {
+    let messages = this.props.messages;
+    if (messages) {
+      return(
+        <div id="messagelist" className="messagelist">
+          <ul>
+            {messages.map((message, i) => {
+                return <Paper className="message" key={i}>
+                          <div className="messageHeader">
+                            <p>{message.username} {message.sent}</p>
+                          </div>                          
+                          <div className="messageText">
+                            <p>{message.message}</p>
+                          </div>                          
+                        </Paper>
+              })}
+          </ul>
+        </div>
+      )
+    }
   }
 }
